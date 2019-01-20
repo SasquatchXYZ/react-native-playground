9f8e7eb0faaf97b0859cf3da44bf442d571c163d
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-20 18:51:57 -0500

    Building ToDo List

--- App.js
+++ App.js
@@ -1,55 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {View, Text, StyleSheet, Platform} from 'react-native';
+import Header from './components/header';
+import Footer from './components/footer';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>Do Wild and Crazy Things!!!</Text>
-        <Text style={styles.instructions}>Then SHAKE your phone....</Text>
-        <Text style={styles.instructions}>Test #2.... I have no idea what I want to build here...</Text>
-        <Text style={styles.instructions}>but it works...</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Header/>
+        <View style={styles.content}>
+
+        </View>
+        <Footer/>
       </View>
     );
   }
 }
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#0DF242',
+    backgroundColor: '#F5F5F5',
+    ...Platform.select({
+      ios: {paddingTop: 30}
+    })
   },
-  welcome: {
-    fontSize: 50,
-    textAlign: 'center',
-    margin: 10,
-    color: '#FF7900',
-    fontWeight: '800'
+  content: {
+    flex: 1,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
