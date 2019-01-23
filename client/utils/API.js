91f219a11b0afb9210f458a33c8eb815454c94af
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-23 12:40:10 -0500

    working for android and added configuration to connect to server on iOS

--- client/utils/API.js
+++ client/utils/API.js
@@ -1,12 +1,21 @@
 import axios from 'axios';
 import {DOMAIN} from 'react-native-dotenv';
 import {LOCAL_IP} from 'react-native-dotenv';
+import {Platform} from 'react-native';
+
+// console.log(Platform.OS);
+const DEV_SERVER_URL = Platform.OS === 'android'
+  ? LOCAL_IP
+  : 'localhost';
+
+// console.log(DEV_SERVER_URL);
+
 
 export default {
   getUser: function (accessToken) {
     // console.log(accessToken);
     // console.log(LOCAL_IP);
-    return axios.get(`http://${LOCAL_IP}:3000/api/things/${accessToken}`)
+    return axios.get(`http://${DEV_SERVER_URL}:3000/api/things/${accessToken}`)
     // return axios.get(`https://${DOMAIN}/userinfo`, {headers: {Authorization: `Bearer ${accessToken}`}})
   }
 }
